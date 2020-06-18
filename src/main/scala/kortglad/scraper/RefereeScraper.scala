0f4e4640c85e6060fb9a3fe18cb53597d4d6828b
Morten Andersen-Gott <morten.andersengott@gmail.com>
2020-06-18 23:00:11 +0200

    Ny UI på fotball.no

--- src/main/scala/kortglad/scraper/RefereeScraper.scala
+++ src/main/scala/kortglad/scraper/RefereeScraper.scala
@@ -17,66 +17,86 @@ import org.jsoup.nodes.Document
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.JavaConverters._
+
 /**
   *
   */
-class RefereeScraper(client:Client[IO])(implicit shift:ContextShift[IO]) extends Http4sClientDsl[IO]{
+class RefereeScraper(client: Client[IO])(implicit shift: ContextShift[IO])
+    extends Http4sClientDsl[IO] {
   val fotballBaseUrl = Uri.uri("https://www.fotball.no")
-  def refereeTemplate(fiksId:Int) = (fotballBaseUrl / "fotballdata" / "person"/"dommeroppdrag").+?("fiksId", fiksId)
+  def refereeTemplate(fiksId: Int) =
+    (fotballBaseUrl / "fotballdata" / "person" / "dommeroppdrag")
+      .+?("fiksId", fiksId)
 
-  def scrapeMatch(matchUri: Uri):IO[MatchStat] =
-    client.expect[String](matchUri).map{ doc =>
+  def scrapeMatch(matchUri: Uri): IO[MatchStat] =
+    client.expect[String](matchUri).map { doc =>
       RefereeScraper.parseMatch(matchUri, Jsoup.parse(doc))
     }
 
-  def scrapeMatches(fiksId:Int) =
-    client.expect[String](refereeTemplate(fiksId)).map{ doc =>
-      val (refName, urls) = RefereeScraper.parseMatches(fiksId, Jsoup.parse(doc))
+  def scrapeMatches(fiksId: Int) =
+    client.expect[String](refereeTemplate(fiksId)).map { doc =>
+      val (refName, urls) =
+        RefereeScraper.parseMatches(fiksId, Jsoup.parse(doc))
       (refName, urls.map(x => fotballBaseUrl.withPath(x)))
     }
 
-  def findRefereeStats(fiksId: Int) : IO[RefereeStats] = {
-    scrapeMatches(fiksId).flatMap{
+  def findRefereeStats(fiksId: Int): IO[RefereeStats] = {
+    scrapeMatches(fiksId).flatMap {
       case (uri, matches) =>
-        matches.parTraverse(scrapeMatch).map{ fetched =>
-          val result = fetched.filter(_.tidspunkt.isBefore(LocalDateTime.now)) match {
-            case h :: t => h :: t.takeWhile(_.inCurrentSeason)
-            case x => x
-          }
-          RefereeStats(result,uri)
+        matches.parTraverse(scrapeMatch).map { fetched =>
+          val result =
+            fetched.filter(_.tidspunkt.isBefore(LocalDateTime.now)) match {
+              case h :: t => h :: t.takeWhile(_.inCurrentSeason)
+              case x      => x
+            }
+          RefereeStats(result, uri)
         }
     }
   }
 
-
-
 }
 
 object RefereeScraper {
-  val log:Logger = LoggerFactory.getLogger(getClass)
+  val log: Logger = LoggerFactory.getLogger(getClass)
 
-  def parseMatches(fiksId:Int, document:Document) : (String, List[String]) = {
+  def parseMatches(fiksId: Int, document: Document): (String, List[String]) = {
     val body = document.body()
     val refName = body.select(".fiks-header--person").select("h1 > a").text()
-    val dommerRader = body.select("tr").asScala.filter(_.select("td").asScala.exists(_.text() == "Dommer"))
-    val urls = dommerRader.flatMap(_.select("td > a").asScala.map(_.attr("href")))
-      .filter(_.contains("/kamp/")).toList
+    val dommerRader = body
+      .select("tr")
+      .asScala
+      .filter(_.select("td").asScala.exists(_.text() == "HD"))
+    val urls = dommerRader
+      .flatMap(_.select("td > a").asScala.map(_.attr("href")))
+      .filter(_.contains("/kamp/"))
+      .toList
     log.info(s"Scraping referee named $refName")
     (refName, urls)
   }
 
-  def parseMatch(matchUri:Uri, kampDoc:Document) = {
-    val lag = kampDoc.select("span.match__teamname-img").asScala.map(_.nextElementSibling().text())
+  def parseMatch(matchUri: Uri, kampDoc: Document) = {
+    val lag = kampDoc
+      .select("span.match__teamname-img")
+      .asScala
+      .map(_.nextElementSibling().text())
     val home = lag.head
     val away = lag.drop(1).head
     val dateText = kampDoc.select("span.match__arenainfo-date").text()
-    val tidspunkt = LocalDateTime.parse(dateText, DateTimeFormatter.ofPattern("dd.MM.yyyy HH.mm"))
+    val tidspunkt = LocalDateTime.parse(
+      dateText,
+      DateTimeFormatter.ofPattern("dd.MM.yyyy HH.mm")
+    )
     val matchEvents = kampDoc.select("ul.match__events")
     val yellows = matchEvents.select("span.icon-yellow-card--events")
     val yellowReds = matchEvents.select("span.icon-yellow-red-card--events")
     val reds = matchEvents.select("span.icon-red-card--events")
-    MatchStat(matchUri, tidspunkt, home, away, CardStat(yellows.size(),yellowReds.size(),reds.size()))
+    MatchStat(
+      matchUri,
+      tidspunkt,
+      home,
+      away,
+      CardStat(yellows.size(), yellowReds.size(), reds.size())
+    )
   }
-
 
 }
