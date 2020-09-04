0e7e9e80785152b61662485d1afe4a2414a1945b
Morten Andersen-Gott <morten.andersengott@gmail.com>
2020-09-04 08:33:17 +0200

    Fix scraper

--- src/main/scala/kortglad/server/Main.scala
+++ src/main/scala/kortglad/server/Main.scala
@@ -2,37 +2,61 @@ package kortglad.server
 
 import java.util.concurrent.Executors
 
+import cats.data.{Kleisli, OptionT}
 import cats.effect.{ExitCode, IO, IOApp, Resource}
 import cats.implicits._
 import kortglad.resources.{RefereeResource, StaticResources}
 import kortglad.scraper.RefereeScraper
+import org.http4s.Status.Redirection
+import org.http4s.{Header, HttpRoutes, Request, Status, Uri}
 import org.http4s.client.Client
 import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.headers.Location
 import org.http4s.server.Router
 import org.http4s.implicits._
 
 import scala.concurrent.ExecutionContext
 import org.http4s.server.blaze._
 import org.http4s.server.middleware._
+import org.http4s.util.CaseInsensitiveString
 
 import scala.util.Properties
 
 object Main extends IOApp {
 
-  def routes(bec:ExecutionContext, client:Client[IO]) = {
+  def routes(bec: ExecutionContext, client: Client[IO]) = {
     Router(
       "/" -> new StaticResources[IO](bec).service,
-      "/referee" -> CORS(new RefereeResource(new RefereeScraper(client)).service))
+      "/referee" -> CORS(
+        new RefereeResource(new RefereeScraper(client)).service
+      )
+    )
   }
-
 
   val port = Properties.envOrElse("PORT", "8080").toInt
   val resource = for {
-    bec <- Resource.make(IO(Executors.newCachedThreadPool()))(e => IO(e.shutdown())).map(ExecutionContext.fromExecutor)
+    bec <- Resource
+      .make(IO(Executors.newCachedThreadPool()))(e => IO(e.shutdown()))
+      .map(ExecutionContext.fromExecutor)
     client <- BlazeClientBuilder[IO](ExecutionContext.global).resource
-    r <- BlazeServerBuilder[IO].bindHttp(port, "0.0.0.0").withHttpApp(routes(bec, client).orNotFound).resource
+    r <- BlazeServerBuilder[IO]
+      .bindHttp(port, "0.0.0.0")
+      .withHttpApp(routes(bec, client).orNotFound)
+      .resource
   } yield r
 
   def run(args: List[String]): IO[ExitCode] =
     resource.use(_ => IO.never).as(ExitCode.Success)
+
+//  def httpsRedirect(service: HttpRoutes[IO]): HttpRoutes[IO] = Kleisli { req: Request[IO] =>
+//    val redirectToHttps = req.headers.get(CaseInsensitiveString("X-Forwarded-Proto")).filter(_.value == "http").isDefined
+//    if(redirectToHttps){
+//      Kleisli(_ => OptionT.liftF(
+//        Status.Redirection(Location(Uri.unsafeFromString(s"https://${req.uri.host}${req.uri.path}")))
+//      )
+//      )
+//    } else {
+//      service(req)
+//    }
+//  }
 }
--- src/test/scala/scraper/RefereeScraperTest.scala
+++ src/test/scala/scraper/RefereeScraperTest.scala
@@ -12,27 +12,23 @@ import scala.concurrent.ExecutionContext
 /**
   *
   */
-object RefereeScraperTest extends IOApp{
+object RefereeScraperTest extends IOApp {
 
   private val morten = 2245443
-  private val skjevis = 2552240
-  private val gjermshus = 2316320
-  private val trædal = 2922378
 
-  def dump(stats:RefereeStats) = IO{
-    println("Totalt: " +stats.cardTotals.pretty)
+  def dump(stats: RefereeStats) = IO {
+    println("Totalt: " + stats.cardTotals.pretty)
     println(stats.cardAverages.snittPretty)
     println(s"Antall kamper: ${stats.matches.length}")
   }
 
-  def log(s:String) = IO(println(s))
+  def log(s: String) = IO(println(s))
 
   override def run(args: List[String]): IO[ExitCode] =
     BlazeClientBuilder[IO](ExecutionContext.global).resource.use { client =>
-      val scraper = new RefereeScraper(Logger(true, true, logAction = Some(log _))(client))
+      val scraper =
+        new RefereeScraper(Logger(true, true, logAction = Some(log _))(client))
       scraper.findRefereeStats(morten).flatMap(dump).as(ExitCode.Success)
     }
-
-
 //  println(RefereeScraper.scrapeMatch("/fotballdata/kamp/?fiksId=6653689"))
 }
