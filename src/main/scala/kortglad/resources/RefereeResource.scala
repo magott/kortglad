96a6f983b1bf5a16beb3597a3fd00f0d20dd85e4
Morten Andersen-Gott <morten.andersengott@gmail.com>
2020-10-30 11:49:57 +0100

    Logging parse errors

--- src/main/scala/kortglad/resources/RefereeResource.scala
+++ src/main/scala/kortglad/resources/RefereeResource.scala
@@ -8,18 +8,22 @@ import io.circe.syntax._
 import org.http4s._
 import org.http4s.dsl._
 import org.http4s.circe._
+import org.slf4j.LoggerFactory
 
 
 /**
   *
   */
 class RefereeResource(scraper:RefereeScraper) extends Http4sDsl[IO] {
+  val log = LoggerFactory.getLogger(this.getClass)
 
   val service = HttpRoutes.of[IO] {
     case request @ GET -> Root =>
       Ok("foooo")
     case request @ GET -> Root / IntVar(fiksId) =>
-      Ok(scraper.findRefereeStats(fiksId).map(_.asJson))
+      Ok(scraper.findRefereeStats(fiksId).map(_.asJson).onError{
+        case t => IO(log.error("Unexpected parser error", t))
+      })
   }
 
 
